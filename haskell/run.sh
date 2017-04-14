b452ea4b97d808dc1ff8110dec020bc63a03624e
oleks <oleks@oleks.info>
2017-04-14 14:05:14 +0200

    This is the haskell implementation

--- haskell/run.sh
+++ haskell/run.sh
@@ -6,6 +6,6 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker run \
   --interactive --tty --rm \
-  --volume "${dir}/:/home/ghc/mechanics/" \
-  --workdir "/home/ghc/mechanics/src/" \
-  alpine-haskell-pl-dev:8.0.2_0.2
+  --volume "${dir}/../:/home/ghc/mechanics/" \
+  --workdir "/home/ghc/mechanics/haskell/" \
+  oleks2/alpine-haskell-pl-dev:8.0.2_0.2
