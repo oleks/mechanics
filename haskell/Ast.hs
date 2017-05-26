61d14a11ee29a1c282e437bce783dfaf5c4e4b19
oleks <oleks@oleks.info>
2017-05-26 13:42:28 +0200

    A new Ast, with a pretty printer instead of Show

--- haskell/Ast.hs
+++ haskell/Ast.hs
@@ -1,28 +1,69 @@
-module Ast where
+module Ast (
+  Name,
+  Value,
+  Pattern(..),
+  Expr(..)
+) where
 
-newtype Name
-  = Name String
-  deriving (Eq, Ord, Show)
+import Text.PrettyPrint.HughesPJClass ( Pretty, pPrint )
+import Text.PrettyPrint
 
-newtype Dec
-  = Dec (Name, [Name], Expr)
-  deriving (Eq, Ord, Show)
+type Name = String
+
+type Value = Double
+
+data Pattern
+  = PatName Name
+  | PatVal Value
+  deriving (Show)
 
 data Expr
-  = Call Name [Expr]
-  | Num Double
-  | Neg Expr
-  | Add Expr Expr
-  | Sub Expr Expr
-  | Mul Expr Expr
-  | Div Expr Expr
-  deriving (Eq, Ord, Show)
+  = ExpNam Name
+  | ExpVal Value
+  | FnCall Name [Expr]
+  | ExpNeg Expr
+  | ExpTup [Expr]
+  | ExpAdd Expr Expr
+  | ExpSub Expr Expr
+  | ExpMul Expr Expr
+  | ExpDiv Expr Expr
+  | ExpLet Name Expr Expr
+  | ExpIf Expr Expr Expr
+  deriving (Show)
 
-newtype Prog
-  = Prog ([Dec], Expr)
-  deriving (Eq, Ord, Show)
+binop :: Expr -> String -> Expr -> Doc
+binop e1 op e2 = pPrint e1 <+> text op <+> pPrint e2
 
-data ParserConfig
-  = ParserConfig {
-    curriedNotation :: Bool
-  }
+instance Pretty Expr where
+  pPrint (ExpNam n) = text n
+  pPrint (ExpVal v) = double v
+  pPrint (FnCall n v) = text n <+> (hsep $ map pPrint v)
+  pPrint (ExpNeg e) = char '~' <> pPrint e
+  pPrint (ExpTup es) = hsep $ punctuate (text ",") (map pPrint es)
+  pPrint (ExpAdd e1 e2) = binop e1 "+" e2
+  pPrint (ExpSub e1 e2) = binop e1 "-" e2
+  pPrint (ExpMul e1 e2) = binop e1 "*" e2
+  pPrint (ExpDiv e1 e2) = binop e1 "/" e2
+  pPrint (ExpLet n e1 e2) =
+    text "let" <+> text n <+> text "=" <+>
+      pPrint e1 <+> text "in" <+> pPrint e2
+  pPrint (ExpIf ec et ef) =
+    text "if" <+> pPrint ec <+>
+      text "then" <+> pPrint et <+>
+      text "else" <+> pPrint ef
+
+instance Eq Expr where
+  (==) (ExpNam n) (ExpNam m) = n == m
+  (==) (ExpVal v) (ExpVal w) = v == w
+  (==) (FnCall n v) (FnCall m w) = n == m && v == w
+  (==) (ExpNeg e) (ExpNeg f) = e == f
+  (==) (ExpTup es) (ExpTup fs) = es == fs
+  (==) (ExpAdd e1 e2) (ExpAdd f1 f2) = e1 == f1 && e2 == f2
+  (==) (ExpSub e1 e2) (ExpSub f1 f2) = e1 == f1 && e2 == f2
+  (==) (ExpMul e1 e2) (ExpMul f1 f2) = e1 == f1 && e2 == f2
+  (==) (ExpDiv e1 e2) (ExpDiv f1 f2) = e1 == f1 && e2 == f2
+  (==) (ExpLet n e1 e2) (ExpLet m f1 f2)
+    = n == m && e1 == f1 && e2 == f2
+  (==) (ExpIf ec et ef) (ExpIf fc ft ff)
+    = ec == fc && et == ft && ef == ff
+  (==) _ _ = False
